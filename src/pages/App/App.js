1b6f51d8a549e5c5f2ecc77d1c39f06ca0571ab8
ahmalla <68600561+ahmalla@users.noreply.github.com>
2022-12-06 13:28:06 -0800

    setup app component

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -8,4 +8,24 @@ import Filter from './components/Filter/Filter';
 import NavBar from './components/NavBar/NavBar';
 
 
+function App() {
+  return (
+  <div className="App">
+    <h1 className="text-center mb-3">Characters</h1>
+    <div className="container">
+      <div className="row">
+      Filter component will be placed here
+        <div className="col-lg-8 col-12">
+          <div className="row">
+           Card component will be placed here
+      </div>
+    </div>
+  </div>
+  </div>
+</div>
+  );
+}
+
+export default App;
+
 
