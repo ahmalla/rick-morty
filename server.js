1c48e423dfabb9ad0ab5d43cb237e58b17f76061
ahmalla <68600561+ahmalla@users.noreply.github.com>
2022-12-06 09:31:46 -0800

    auth route begining

--- server.js
+++ server.js
@@ -6,3 +6,9 @@ require('dotenv').config();
 const app = express();
 
 app.use(logger('dev'));
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, function() {
+    console.log(`Express app running on port ${port}`);
+  });
