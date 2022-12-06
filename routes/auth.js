4b5d88bf977e548c47a1c1948cc20dd4ca05a28c
ahmalla <68600561+ahmalla@users.noreply.github.com>
2022-12-06 11:24:55 -0800

    hashing password

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,16 @@
 const router = require('express').Router();
 const User = require('../model/User');
-
+const bcrypt = require('bcrypt');
 router.post('/register', async (req, res) => {
+    // checking if user already exists
+    const emailExists = await User.findOne({email: req.body.email});
+    if(emailExists) return res.status(400).send('Email already exists');
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+    // Create a new user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
