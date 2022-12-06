ef204b0f467a92ee7b33c2def16d681676f38e00
ahmalla <68600561+ahmalla@users.noreply.github.com>
2022-12-06 12:52:24 -0800

    App.js imports setup

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -3,7 +3,9 @@ import "bootstrap/dist/js/bootstrap";
 import React, { useState, useEffect } from "react";
 import Search from './components/Search/Search';
 import Card from './components/Card/Card';
-
+import Pagination from './components/Pagination/Pagination';
+import Filter from './components/Filter/Filter';
+import NavBar from './components/NavBar/NavBar';
 
 
 
